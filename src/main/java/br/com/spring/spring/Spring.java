d3bb676cb59aa9c4cf1069d7e352790554a11e91
Fernando José dos Santos - FJO <fernando.jose@iteris.local>
2020-01-31 16:34:53 -0300

    first commit

---
+++ src/main/java/br/com/spring/spring/Spring.java
@@ -0,0 +1,13 @@
+package br.com.spring.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring.class, args);
+	}
+
+}
